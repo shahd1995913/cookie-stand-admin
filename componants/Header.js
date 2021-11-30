0b93913054be4ece11f0b7fba290ad9ead4c5829
shahd1995913 <shahdalkhatib95@gmail.com>
2021-11-30 06:29:35 +0400

    lab 37

---
+++ componants/Header.js
@@ -0,0 +1,12 @@
+
+export default function Header() {
+
+    return (
+        
+        <header className="p-4 bg-green-400 text-black-50">
+         
+            <h2 className="text-4xl">Cookie Stand Admin</h2>
+        
+        </header>
+    )
+}
